b6553cb49978382cd19be319dbe4817591eac9fc
Niels Lange <info@nielslange.de>
2018-03-22 20:51:28 +0700

    Replace recent blog posts on homepage with three selected posts

--- wp-content/themes/sushi-lover/template-parts/home-blog.php
+++ wp-content/themes/sushi-lover/template-parts/home-blog.php
@@ -11,6 +11,9 @@
 //* Return if ACF hasn't been activated
 if ( !class_exists('acf') ) return;
 
+//* Prepare variables
+$title = get_field('home_blog_title');
+
 //* Prepare recent blog posts
 $count = get_field('home_blog_visible_posts');
 $args = array(
@@ -25,41 +28,46 @@ $recent_posts = wp_get_recent_posts($args);
 <div id="home-blog" class="space">
 	<div class="container">
 
-        <?php if ( get_field('home_blog_title') ) : ?>
-            <h2><?php the_field('home_blog_title'); ?></h2>
+        <?php if ( $title ) : ?>
+            <h2><?php echo $title; ?></h2>
         <?php endif; ?>
 
-		<div class="row hidden-xs">
-			<?php $i = 1; foreach ($recent_posts as $post) : ?>
-			<div class="col-sm-4">
-				<?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post['ID']), 'blog_horizontal'); ?>
-                <ul class="caption-style-1">
-                    <li>
-                        <img src="<?php echo $thumb[0]; ?>" alt="<?php echo $post['post_excerpt'];?>" class="img-responsive">
-                        <div class="caption">
-                            <div class="blur"></div>
-                            <div class="caption-text">
-                                <h1><?php echo $post['post_title']; ?></h1>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-			</div>
-			<?php if ($i % 3 == 0 && $i != $count) : ?>
-			</div><div class="row">
-			<?php endif; ?>
-			<?php $i++; endforeach; ?>
-		</div>
-	
-        <div class="row visible-xs">
-            <?php $i = 1; foreach ($recent_posts as $post) : ?>
-            <div class="col-xs-12">
-                <?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post['ID']), 'full'); ?>
-                <p><a href="<?php echo get_permalink($post['ID']);?>"><img src="<?php echo $thumb[0]; ?>" alt="<?php echo $post['post_excerpt'];?>" class="img-responsive"></a></p>
-                <p><a href="<?php echo get_permalink($post['ID']);?>"><?php echo $post['post_title']; ?></a></p>
+        <?php if ( have_rows('home_blog_posts') ) : ?>
+
+            <div class="row visible-xs">
+                <?php while ( have_rows('home_blog_posts') ) : the_row(); ?>
+                    <?php $id = get_sub_field('home_blog_post'); ?>
+                    <div class="col-xs-12">
+			            <?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($id), 'full'); ?>
+                        <p><a href="<?php echo get_permalink($id);?>"><img src="<?php echo $thumb[0]; ?>" alt="<?php echo get_the_excerpt($id);?>" class="img-responsive"></a></p>
+                        <p><a href="<?php echo get_permalink($id);?>"><?php echo get_the_title($id); ?></a></p>
+                    </div>
+                <?php endwhile; ?>
             </div>
-            <?php $i++; endforeach; ?>
-        </div>
+
+            <div class="row hidden-xs">
+	            <?php while ( have_rows('home_blog_posts') ) : the_row(); ?>
+		            <?php $id = get_sub_field('home_blog_post'); ?>
+                    <div class="col-sm-4">
+			            <?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($id), 'blog_horizontal'); ?>
+                        <ul class="caption-style-1">
+                            <li>
+                                <a href="<?php echo get_permalink($id);?>">
+                                    <img src="<?php echo $thumb[0]; ?>" alt="<?php echo get_the_excerpt($id);?>" class="img-responsive">
+                                    <div class="caption">
+                                        <div class="blur"></div>
+                                        <div class="caption-text">
+                                            <h1><?php echo get_the_title($id); ?></h1>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+	            <?php endwhile; ?>
+            </div>
+
+		<?php endif; ?>
 
 		<?php wp_reset_query(); ?>
         
